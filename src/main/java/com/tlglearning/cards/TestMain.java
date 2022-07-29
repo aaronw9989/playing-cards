123bfebbe055609977ee605bc5acaef8cda77949
aaronw9989 <williamsa493@strose.edu>
2022-07-29 15:45:45 -0500

    Moved rank-first comparator as local class in main method.

--- src/main/java/com/tlglearning/cards/TestMain.java
+++ src/main/java/com/tlglearning/cards/TestMain.java
@@ -24,22 +24,24 @@ public class TestMain {
     deck.sort();
     System.out.println(deck);
 
+    // Local class because inside a method
+    class RankFirstComparator implements Comparator<Card> {
+
+      @Override
+      public int compare(Card card1, Card card2) {
+
+        int comparison = card1.getRank().compareTo(card2.getRank());
+        if (comparison == 0) {
+          comparison = card1.getSuit().compareTo(card2.getSuit());
+        }
+        return comparison;
+      }
+
+    }
+
     deck.sort(new RankFirstComparator());
     System.out.println(deck);
 
   }
 
-  static class RankFirstComparator implements Comparator<Card> {
-
-    @Override
-    public int compare(Card card1, Card card2) {
-
-      int comparison = card1.getRank().compareTo(card2.getRank());
-      if (comparison == 0) {
-        comparison = card1.getSuit().compareTo(card2.getSuit());
-      }
-      return comparison;
-    }
-
-  }
 }
