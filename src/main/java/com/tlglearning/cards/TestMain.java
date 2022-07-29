527f12a1a24f85cbf9273009eafd341a96377639
aaronw9989 <williamsa493@strose.edu>
2022-07-29 15:54:18 -0500

    Rank-first comparator as a statement lambda in main.

--- src/main/java/com/tlglearning/cards/TestMain.java
+++ src/main/java/com/tlglearning/cards/TestMain.java
@@ -26,22 +26,17 @@ public class TestMain {
 
     // Local class because inside a method
 
-    deck.sort(new Comparator<Card>() {
-
-      @Override
-      public int compare(Card card1, Card card2) {
-
-        int comparison = card1.getRank().compareTo(card2.getRank());
-        if (comparison == 0) {
-          comparison = card1.getSuit().compareTo(card2.getSuit());
-        }
-        return comparison;
+    deck.sort((card1, card2) -> {
+      int comparison = card1.getRank().compareTo(card2.getRank());
+      if (comparison == 0) {
+        comparison = card1.getSuit().compareTo(card2.getSuit());
       }
-      });
+      return comparison;
+    });
 
     System.out.println(deck);
 
-    }
-
   }
+
+}
 
